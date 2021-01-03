04984ce17e90ab82d2cda1389e495db7cd3e75ea
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-01-03 21:09:05 +0900

    Add jsdoc with type system

--- project/app.js
+++ project/app.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 // utils
 function $(selector) {
   return document.querySelector(selector);
@@ -36,11 +38,24 @@ function createSpinnerElement(id) {
 let isDeathLoading = false;
 let isRecoveredLoading = false;
 
+/**
+ * @typedef {object} CovidSummary
+ * @property {Array<object>} Country
+ * 
+ */
 // api
+/**
+ * @returns {Promise<CovidSummary>}
+ * 
+ * 
+ */
 function fetchCovidSummary() {
   const url = 'https://api.covid19api.com/summary';
   return axios.get(url);
 }
+fetchCovidSummary().then(res=>{
+  res.Country
+})
 
 function fetchCountryInfo(countryCode, status) {
   // params: confirmed, recovered, deaths
